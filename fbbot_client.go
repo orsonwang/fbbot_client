e585c7287934d816e1ee1abca882f386ae3e8a96
orsonwang <orsonwang@gmail.com>
2016-06-03 17:38:44 +0800

    move test service to backend

--- fbbot_client.go
+++ fbbot_client.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	logger "log"
 	"net/http"
 	"os"
 
+	"github.com/nats-io/nats"
 	"github.com/orsonwang/fbot"
 )
 
 var mainFBEventHandler *FBBotEventHandler
 var mainFBBotClient *fbot.Bot
+var log *logger.Logger
+var nc *nats.Conn
 
 func main() {
+	f, err := os.OpenFile("./fbbot.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	if err != nil {
+		log.Fatalf("Can't connect: %v\n", err)
+	}
+	log = new(logger.Logger)
+	log.SetOutput(f)
+
+	urls := "nats://localhost:4222"
+	nc, err = nats.Connect(urls)
+	if err != nil {
+		log.Fatalf("Can't connect: %v\n", err)
+	}
+	defer nc.Close()
 
 	mainFBBotClient = fbot.NewBot(fbot.Config{
 		AccessToken: os.Getenv("FB_PAGE_TOKEN"),
